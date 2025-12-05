3cd5ae51d81f24fd09ce6c33f118602400f2c5af
Amaar Abd <abdamaar64@gmail.com>
2025-12-05 13:36:58 +0300

    create splach view

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,9 @@
+
+class Assets {
+  // Assets._();
+  static const String assetsImagesCircles = "assets/images/Circles.svg";
+  static const String assetsImagesFreepik = "assets/images/freepik.svg";
+  static const String assetsImagesLogo = "assets/images/logo.svg";
+  static const String assetsImagesLogoApp = "assets/images/logo_app.png";
+}
+
--- lib/features/splash/presentation/views/widgets/splach_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splach_view_body.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/utils/app_images.dart';
+import 'package:svg_flutter/svg.dart';
 
 class SplachViewBody extends StatelessWidget {
   const SplachViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Column(children: []));
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Align(
+            alignment: AlignmentGeometry.topLeft,
+            child: SvgPicture.asset(Assets.assetsImagesFreepik),
+          ),
+          SvgPicture.asset(Assets.assetsImagesLogo),
+          SvgPicture.asset(Assets.assetsImagesCircles, fit: BoxFit.fill),
+        ],
+      ),
+    );
   }
 }
