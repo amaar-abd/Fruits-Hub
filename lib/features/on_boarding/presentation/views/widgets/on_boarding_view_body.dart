867e1a8967901bc8bae5f8f47267f3adea67c15a
Amaar Abd <abdamaar64@gmail.com>
2025-12-06 23:55:05 +0300

    finished ui on_boarding_view

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,4 +1,7 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/utils/app_colors.dart';
+import 'package:fruits_hub/core/widgets/custom_button.dart';
 import 'package:fruits_hub/features/on_boarding/presentation/views/widgets/on_boarding_pages_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
@@ -6,9 +9,23 @@ class OnBoardingViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Expanded(child: OnBoardingPagesView()),
-
-    ]);
+    return Column(
+      children: [
+        Expanded(child: OnBoardingPagesView()),
+        DotsIndicator(
+          dotsCount: 2,
+          decorator: DotsDecorator(
+            activeColor: AppColors.primaryColor,
+            color: AppColors.primaryColor.withValues(alpha: .5),
+          ),
+        ),
+        SizedBox(height: 30),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: CustomButton(onPressed: () {}, text: 'ابدأ الان'),
+        ),
+        SizedBox(height: 44),
+      ],
+    );
   }
 }
